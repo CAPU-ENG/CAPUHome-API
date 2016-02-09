ef80e7afbdf9afdc400b2a825be9313f0b959d15
JeldorPKU <jack.jiang.capu@gmail.com>
2016-02-09 16:08:25 +0800

    Remove useless comments.

--- app/models.py
+++ app/models.py
@@ -11,19 +11,15 @@ from app import db
 class User(db.Model):
     __tablename__ = 'users'
 
-    # new segments
     uid = db.Column(db.Integer, primary_key=True)
     birthday = db.Column(db.Date)
     student_id = db.Column(db.String(16))
     name = db.Column(db.String(16))
     tel = db.Column(db.String(16))
 
-    # existing segments
     username = db.Column(db.String(32), unique=True)
     password = db.Column(db.String(32), nullable=False)
-    # token temporarily not included
-    # tokentime temporarily not included
-    gender = db.Column(db.Boolean, nullable=False)  # was `sex`
+    gender = db.Column(db.Boolean, nullable=False)
     avatar = db.Column(db.String(32))
     intro = db.Column(db.Text)
     sig1 = db.Column(db.Text)
@@ -33,24 +29,17 @@ class User(db.Model):
     qq = db.Column(db.String(16))
     mail = db.Column(db.String(64))
     wechat = db.Column(db.String(64))
-    province = db.Column(db.String(8))  # was `place`
-    registration_date = db.Column(db.Date)  # was `regdate`
-    last_login_time = db.Column(db.DateTime)  # was `lastdate`
-    last_post_time = db.Column(db.DateTime)  # was `lastpost`
+    province = db.Column(db.String(8))
+    registration_date = db.Column(db.Date)
+    last_login_time = db.Column(db.DateTime)
+    last_post_time = db.Column(db.DateTime)
     star = db.Column(db.Integer, default=1)
-    # score temporarily not included
-    num_post = db.Column(db.Integer, unsigned=True)  # was `post`
-    num_reply = db.Column(db.Integer, unsigned=True)  # was `reply`
-    num_water = db.Column(db.Integer, unsigned=True)  # was `water`
-    num_sign = db.Column(db.Integer, unsigned=True)  # was `sign`
-    # rights temporarily not included
-    # newmsy temporarily not included
-    # extr temporarily not included
-    current_board = db.Column(db.Integer)  # was `nowboard`
-    user_agent = db.Column(db.String(128))  # was `onlinetype`
-    # logininfo temporarily not included
-    # code temporarily not included
-    # other2 3 4 5 6 temporarily not included
+    num_post = db.Column(db.Integer, unsigned=True)
+    num_reply = db.Column(db.Integer, unsigned=True)
+    num_water = db.Column(db.Integer, unsigned=True)
+    num_sign = db.Column(db.Integer, unsigned=True)
+    current_board = db.Column(db.Integer)
+    user_agent = db.Column(db.String(128))
 
 
 class Group(db.Model):
@@ -67,8 +56,8 @@ class Board(db.Model):
     __tablename__ = 'boards'
 
     bid = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), unique=True)  # was `bbstitle`
-    invisible = db.Column(db.Boolean, default=False)  # was `hide`
+    name = db.Column(db.String(32), unique=True)
+    invisible = db.Column(db.Boolean, default=False)
 
 
 class Threads(db.Model):
@@ -78,45 +67,41 @@ class Threads(db.Model):
     bid = db.Column(db.Integer)
     title = db.Column(db.String)
     content = db.Column(db.Text)
-    author_uid = db.Column(db.Integer)  # was `author`
-    replyer_uid = db.Column(db.Integer)  # was `replyer`
-    num_click = db.Column(db.Integer, default=0)  # was `click`
-    num_reply = db.Column(db.Integer, default=0)  # was `reply`
-    # gesture temporarily not included
-    good = db.Column(db.Boolean, default=False)  # was `extr`
-    sticky = db.Column(db.Boolean, default=False)  # was `top`
-    created_at = db.Column(db.DateTime)  # was `postdate`
-    replied_at = db.Column(db.DateTime)  # was `timestamp`
+    author_uid = db.Column(db.Integer)
+    replyer_uid = db.Column(db.Integer)
+    num_click = db.Column(db.Integer, default=0)
+    num_reply = db.Column(db.Integer, default=0)
+    good = db.Column(db.Boolean, default=False)
+    sticky = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime)
+    replied_at = db.Column(db.DateTime)
 
 
 class Post(db.Model):
     __tablename__ = 'posts'
 
-    pid = db.Column(db.Integer, primary_key=True)  # was `fid`
+    pid = db.Column(db.Integer, primary_key=True)
     bid = db.Column(db.Integer)
     tid = db.Column(db.Integer)
     title = db.Column(db.String(32))
-    uid = db.Column(db.Integer)  # was `author`
-    content = db.Column(db.Text)  # was `text`
-    # ishtml temporarily not included
-    # attachs temporarily not included
-    created_at = db.Column(db.DateTime)  # was `replytime`
-    updated_at = db.Column(db.DateTime)  # was `updatetime`
+    uid = db.Column(db.Integer)
+    content = db.Column(db.Text)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
     signature = db.Column(db.Text)
-    user_agent = db.Column(db.String(128))  # was `type`
+    user_agent = db.Column(db.String(128))
     ip = db.Column(db.String(64))
-    # lzl temporarily not included
 
 
 class Comment(db.Model):
     __tablename__ = 'comments'
 
-    cid = db.Column(db.Integer, primary_key=True)  # was `id`
-    pid = db.Column(db.Integer)  # was `fid`
-    uid = db.Column(db.Integer)  # was `author`
-    content = db.Column(db.Text)  # was `text`
+    cid = db.Column(db.Integer, primary_key=True)
+    pid = db.Column(db.Integer)
+    uid = db.Column(db.Integer)
+    content = db.Column(db.Text)
     time = db.Column(db.DateTime)
-    deleted = db.Column(db.Boolean, default=False)  # was `visible`
+    deleted = db.Column(db.Boolean, default=False)
 
 
 class Message(db.Model):
@@ -128,7 +113,6 @@ class Message(db.Model):
     content = db.Column(db.Text)
     time = db.Column(db.DateTime)
     is_read = db.Column(db.Boolean, default=False)
-    # ruser, rmsg, rbid, rtid, rpid deleted
     sender_deleted = db.Column(db.Boolean, default=False)
     receiver_deleted = db.Column(db.Boolean, default=False)
 
